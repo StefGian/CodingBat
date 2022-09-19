327b99d3d847f9bdc4186ab7849bb8aba91d5da7
Stefania Giannopoulou <stefania.gian91@gmail.com>
2022-09-19 09:47:37 +0300

    Add files via upload

---
+++ MakeChocolate.java
@@ -0,0 +1,48 @@
+public class MakeChocolate {
+
+    /*
+     * We want make a package of goal kilos of chocolate. We have small bars (1 kilo each) and big bars (5 kilos each). 
+     * Return the number of small bars to use, assuming we always use big bars before small bars. Return -1 if it can't be done.
+    makeChocolate(4, 1, 9) → 4
+    makeChocolate(4, 1, 10) → -1
+    makeChocolate(4, 1, 7) → 2
+     */
+
+
+    public static void main (String[] args){
+        int result = makeChocolate(1, 2, 5);
+        System.out.println("result: " + result);
+
+    }
+
+
+
+    public static int makeChocolate(int small, int big, int goal){
+        int result = 0;
+
+        int remainder = goal;
+
+        for (int i = 0; i <= big; i++){
+            if(goal - (i*5) < 0){
+                break;
+            }
+            remainder = goal - (i*5);
+            System.out.println("remainder: " + remainder);
+            
+        }
+
+        
+
+        if (remainder <= small){
+            result = remainder;
+        }else{
+            result = -1;
+        }
+
+
+        return result;
+    }
+
+
+    
+}
